ba9ce70900dccec0ea6d30d02bcbd59883874335
abhay singh <abhay.singh@exilant.com>
2017-07-05 12:01:55 +0530

    Initial Commit

---
+++ JojoPizzaNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JojoPizzaNotification
+//
+//  Created by abhay singh on 05/07/17.
+//  Copyright © 2017 abhay singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
